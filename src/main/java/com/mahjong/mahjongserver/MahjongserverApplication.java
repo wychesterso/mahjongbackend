970a85285d468dcc0a3f2bf060f372699af103b8
Chester So <chesterwyso@gmail.com>
2025-07-09 19:55:11 +1000

    First commit, Spring template

---
+++ src/main/java/com/mahjong/mahjongserver/MahjongserverApplication.java
@@ -0,0 +1,13 @@
+package com.mahjong.mahjongserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MahjongserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MahjongserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mahjong/mahjongserver/MahjongserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahjong.mahjongserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MahjongserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
