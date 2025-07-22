427a566f17fc6e929d54299022d1d5813028114f
Chester So <chesterwyso@gmail.com>
2025-07-22 22:23:15 +1000

    Fixed web socket config

--- src/main/java/com/mahjong/mahjongserver/config/WebSocketConfig.java
+++ src/main/java/com/mahjong/mahjongserver/config/WebSocketConfig.java
@@ -1,16 +1,21 @@
 package com.mahjong.mahjongserver.config;
 
-import com.mahjong.mahjongserver.websocket.GameWebSocketHandler;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new GameWebSocketHandler(), "/game")
-                .setAllowedOrigins("*");
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic", "/queue");
+        config.setApplicationDestinationPrefixes("/app");
     }
 }
